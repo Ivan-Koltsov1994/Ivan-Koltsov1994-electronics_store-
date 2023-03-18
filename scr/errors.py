bd5ad913145a75ea5b047b915d9f3de9a4fb7b4e
Ivan <ivan.koltsov.1994@mail.com>
2023-03-18 20:58:08 +0500

    Выполнил задание модуля 6: - Доработал тесты

--- scr/errors.py
+++ scr/errors.py
@@ -2,6 +2,3 @@ class InstantiateCSVError(Exception):
     """Класс-исключение для ошибок, связанных с повреждением файла"""
     def __init__(self, *args):
         self.message = args[0] if args else "Неизвестная ошибка"
-
-    def __str__(self):
-        return self.message
