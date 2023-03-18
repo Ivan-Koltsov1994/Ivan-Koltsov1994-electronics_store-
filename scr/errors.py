35a319dfeda2fe22e2b6eb1fde6a11c74cee2c42
Ivan <ivan.koltsov.1994@mail.com>
2023-03-18 20:59:57 +0500

    - Доработал тесты

--- scr/errors.py
+++ scr/errors.py
@@ -1,4 +1,5 @@
 class InstantiateCSVError(Exception):
     """Класс-исключение для ошибок, связанных с повреждением файла"""
+
     def __init__(self, *args):
         self.message = args[0] if args else "Неизвестная ошибка"
